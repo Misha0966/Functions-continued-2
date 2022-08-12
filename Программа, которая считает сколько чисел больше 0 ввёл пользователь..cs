a862c236ae424c99460294d27472b2a271d23a2b
Misha0966 <108358743+Misha0966@users.noreply.github.com>
2022-08-12 14:32:55 +0300

    Update Программа, которая считает сколько чисел больше 0 ввёл пользователь..cs

--- Программа, которая считает сколько чисел больше 0 ввёл пользователь..cs
+++ Программа, которая считает сколько чисел больше 0 ввёл пользователь..cs
@@ -2,11 +2,11 @@
 
 // 1, -7, 567, 89, 223-> 3
 
-Console.Write($"Введите число M, которому соответствует количество чисел: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[] massiveNumbers = new int[n];
+Console.Write($"Введите число M, которому соответствует количество чисел: "); //условие задачи
+int n = Convert.ToInt32(Console.ReadLine()); // задаём переменную n и присваиваем ей следующие значения
+int[] massiveNumbers = new int[n]; // задаём массив
 
-void InputNumbers(int n){
+void InputNumbers(int n){ //метод поска числа
 for (int i = 0; i < n; i++)
   {
     Console.Write($"Введите {i+1} число: ");
@@ -15,7 +15,7 @@ for (int i = 0; i < n; i++)
 }
 
 
-int Comparison(int[] massiveNumbers)
+int Comparison(int[] massiveNumbers) //метод создания массива
 {
   int count = 0;
   for (int i = 0; i < massiveNumbers.Length; i++)
@@ -27,4 +27,4 @@ int Comparison(int[] massiveNumbers)
 
 InputNumbers(n);
 
-Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} "); // конечный результат
